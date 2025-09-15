1515be0ac8a1444b005b01df2b35b6ecfc1c9bca
Antoine Leeman <antoineleeman@gmail.com>
2025-09-15 13:08:50 +0200

    Added new files for code generation

---
+++ generate_osqp_rockETH.py
@@ -0,0 +1,30 @@
+# generate_osqp_integrator.py
+from dyn.rockETH.rockETH import RockETH
+from dyn.LTV import LTV
+from solver.qp_jit import QP
+from dyn.integrator import Integrator
+import numpy as np
+# delete the build directory if it exists
+import shutil
+import os
+import argparse
+build_dir = "build/osqp_fast"
+if os.path.exists(build_dir):
+    shutil.rmtree(build_dir)
+
+
+m = RockETH()
+
+Q = np.eye(m.nx)
+R = np.eye(m.nu)
+
+Qf = 10 * Q
+parser = argparse.ArgumentParser()
+parser.add_argument('--N', type=int, default=15, help='Horizon length')
+args = parser.parse_args()
+N = int(args.N)
+
+m_LTV = LTV(m, N)
+qp = QP(N, Q, R, m_LTV, Qf, backend="osqp")  # uses Python OSQP just to export
+qp.export_osqp_solver("build/osqp_fast", extension_name="osqp_generated")
+print("Wrote code to build/osqp_fast")
